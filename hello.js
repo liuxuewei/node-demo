00bdf6e959e6d8475cef95723cb8b9e4f906cc32
liuxuewei <xuewei.lxw@alibaba-inc.com>
2021-12-26 12:42:32 +0800

    添加promise

--- hello.js
+++ hello.js
@@ -1,13 +1,12 @@
 console.log('执行hello.js')
 
 
-// const setHello = function(name){
-//     return `名字叫：${name}`
-// }
+const setHello = function(name){
+    return `名字叫：${name}`
+}
 exports.sayHello = function(name){
     const str = setHello(name);
     console.log(`执行sayHello ${str}`);
 }
 
-const setName = {}
 
